d9e19196e5f29ffd83b5226bb1f3f068a1c2a564
Faith <pfaithmtan@gmail.com>
2019-08-06 14:02:18 -0700

    Implement _.every()
    - Use _.identity() for case where no callback is provided
    - Use _.reduce() for general case

--- src/underbar.js
+++ src/underbar.js
@@ -206,9 +206,13 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
-    // return _.reduce(collection, function(item) {
-    //   return iterator(item);
-    // }, true);
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(isTrue, item) {
+      return (isTrue && !!iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
